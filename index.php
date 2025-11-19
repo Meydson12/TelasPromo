36fdfb4b42d933da7495b90e17e48dc2d47c55d9
Mey Almeida <meyalmei@gmail.com>
2025-11-19 15:39:11 -0400

    Tela de Login

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login.css">
+
+    <title>Telas Promo</title>
+</head>
+
+<body>
+
+    <div class="login-card">
+        <h2>Login</h2>
+        <h3>Adicione suas credênciais</h3>
+        <form class="login-form">
+            <input type="text" placeholder="Nome de usuario">
+            <input type="password" placeholder="Senha">
+            <button type="submit">Avançar</button>
+
+        </form>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
